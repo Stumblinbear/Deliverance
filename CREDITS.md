6f1ff1618e7de718d361d1f7fdad77ed6675f6a6
Stumblinbear <stumblinbear@gmail.com>
2021-03-04 20:58:16 -0600

    Group ships by manufacturers

---
+++ CREDITS.md
@@ -0,0 +1,3 @@
+<div>Icons made by <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
+
+<div>Icons made by <a href="https://www.flaticon.com/authors/dinosoftlabs" title="DinosoftLabs">DinosoftLabs</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
--- src/views/Ships.vue
+++ src/views/Ships.vue
@@ -4,12 +4,25 @@
       {{ ships }}
     </v-alert>
     <v-row v-else>
-        <template v-if="!ships.data">
-            <v-col v-for="i in 4" :key="i"
-                    cols="12" sm="6" md="4">
-                <v-skeleton-loader type="card-heading, list-item, list-item" />
-            </v-col>
-        </template>
+        <v-col v-if="!ships.data">
+            <v-row>
+                <v-col cols="12">
+                    <v-list-item>
+                        <v-list-item-avatar>
+                            <v-skeleton-loader type="avatar" />
+                        </v-list-item-avatar>
+                        <v-list-item-content>
+                            <v-skeleton-loader type="heading" />
+                        </v-list-item-content>
+                    </v-list-item>
+                </v-col>
+
+                <v-col v-for="i in 4" :key="i"
+                        cols="12" sm="6" md="4">
+                    <ship-info />
+                </v-col>
+            </v-row>
+        </v-col>
         <v-col v-else-if="ships.data.ships.length == 0">
             <v-card>
                 <v-card-text>
@@ -17,112 +30,41 @@
                 </v-card-text>
             </v-card>
         </v-col>
-        <v-col v-else
-                v-for="(ship, i) in ships.data.ships.sort((a, b) => a.type.localeCompare(b.type))" :key="i"
-                cols="12" sm="6" md="4">
-            <v-card class="fill-height">
-                <v-card-text class="pb-0">
-                    <v-row align="center">
-                        <v-col class="shrink">
-                            <div v-if="SHIPS[ship.type]"
-                                    class="ship-img">
-                                <div v-for="(part, i) in SHIPS[ship.type]" :key="i"
-                                    class="ship-part" :style="tile(...part)" />
-                            </div>
-                        </v-col>
-                        <v-col>
-                            {{ ship.type }}
-                        </v-col>
-                        <v-col class="text-right">
-                            <v-chip small label>
-                                {{ ship.manufacturer }}
-                            </v-chip>
-                        </v-col>
-                    </v-row>
-
-                    <!-- Removed because the types are just the Manufacturer + Class
-                        <div class="text-h5 text--primary">
-                            {{ ship.type }}
-                        </div>
-                    -->
-                </v-card-text>
-
-                <v-list-item>
-                    <div>Speed</div>
-                    
-                    <v-divider class="mx-3" />
-
-                    <span v-for="i in ship.speed" :key="i"
-                        class="pa-2 mr-1 primary rounded-circle d-inline-block" />
-                    <span v-for="i in (3 - ship.speed)" :key="i"
-                        class="pa-2 mr-1 grey rounded-circle d-inline-block" />
-                </v-list-item>
-                <v-list-item v-for="[ name, id ] in [ ['Max Cargo', 'maxCargo'], ['Plating', 'plating'], ['Weapons', 'weapons'] ]" :key="id">
-                    <div>{{ name }}</div>
-                    
-                    <v-divider class="mx-3" />
-
-                    <v-chip small label>
-                        {{ ship[id] }}
-                    </v-chip>
-                </v-list-item>
-
-                <v-card-actions class="align-self-end">
-                    <v-chip>
-                        {{ ship.purchaseLocations.reduce((acc, loc) => (!acc || loc.price &lt; acc ? loc.price : acc), null) }} Credits
-                    </v-chip>
-
-                    <v-divider class="ml-2 mr-5" />
-                    
-                    <v-btn depressed color="primary">
-                        Purchase
-                    </v-btn>
-                </v-card-actions>
-            </v-card>
+        <v-col v-else>
+            <v-row v-for="(manufacturer, i) in manufacturers"
+                    :key="'manufacturer-' + i">
+                <v-col cols="12">
+                    <v-list-item>
+                        <v-list-item-avatar rounded="0">
+                            <v-img :src="'/assets/manufacturers/' + manufacturer.name.toLowerCase() + '.png'" />
+                        </v-list-item-avatar>
+                        <v-list-item-content>
+                            <v-list-item-title>{{ manufacturer.name }}</v-list-item-title>
+                            <v-list-item-subtitle>{{ manufacturer.info }}</v-list-item-subtitle>
+                        </v-list-item-content>
+                    </v-list-item>
+                </v-col>
+                <v-col v-for="ship in manufacturer.ships" :key="ship.type"
+                        cols="12" sm="6" md="4">
+                    <ship-info :ship="ship" />
+                </v-col>
+            </v-row>
         </v-col>
     </v-row>
 </template>
 
-<style>
-    .ship-img {
-        position: relative;
-        width: 32px;
-        height: 32px;
-    }
-    .ship-part {
-        position: absolute;
-        top: 0;
-        left: 0;
-
-        width: 100%;
-        height: 100%;
-
-        background-image: url('/assets/ships.png');
-        background-repeat: no-repeat;
-
-        -ms-interpolation-mode: nearest-neighbor;
-        image-rendering: crisp-edges;
-        image-rendering: pixelated;
-    }
-</style>
-
 <script>
-    const SHIPS = {
-        'EM-MK-II': [ [ 0, 3, 0, 0 ], [ 0, 4, 0, 0 ], [ 8, 6, 0, -7 ], [ 1, 1, 0, -5 ] ],
-        'EM-MK-III': [ [ 5, 3, 0, 0 ], [ 0, 5, 0, 4 ], [ 7, 6, 0, -7 ], [ 2, 1, 0, 0 ] ],
-        
-        'GR-MK-I': [ [ 2, 1, 0, 0 ], [ 2, 4, 0, 2 ], [ 13, 0, 0, 0 ] ],
-        'GR-MK-II': [ [ 5, 1, 0, 0 ], [ 3, 4, 0, -2 ], [ 14, 1, 0, -5 ] ],
-        'GR-MK-III': [ [ 4, 5, 0, 4 ], [ 3, 2, 0, 0 ], [ 15, 0, 0, 2 ] ],
-        
-        'JW-MK-I': [ [ 2, 4, 0, 0 ], [ 2, 6, 0, -6 ], [ 8, 0, 0, 0 ] ],
-        'JW-MK-II': [ [ 3, 4, 0, 2 ], [ 1, 6, 0, -3 ], [ 3, 6, 0, -10 ], [ 7, 0, 0, 0 ] ],
-        
-        'ZA-MK-II': [ [ 9, 3, 0, 0 ], [ 2, 5, 0, -1 ], [ 13, 0, 0, -3 ] ],
-        'ZA-MK-III': [ [ 8, 3, 0, -3 ], [ 2, 5, 0, 1 ], [ 15, 0, 0, 0 ] ],
+    import ShipInfo from '@/components/ShipInfo.vue';
+
+    const MANUFACTURERS = {
+        'ELECTRUM': { info: 'Specializing in quick, agile fighter crafts. \'Cause you\'ve got a need for speed.' },
+        'GRAVAGER': { info: 'Take \'yer shit where \'yer shit need t\' be took.' },
+        'JACKSHAW': { info: 'Spacecraft at affordable rates. (disclaimer: not responsible for spontneous combustion)' },
+        'ZETRA': { info: 'Balanced spaceships at unbalanced prices.' },
     };
 
     export default {
+        components: { ShipInfo },
         chimera: {
             user: {
                 url: '/users/Stumblinbear',
@@ -133,10 +75,36 @@
                 interval: 1000 * 60
             }
         },
-        data: () => ({ SHIPS }),
-        methods: {
-            tile(u, v, x, y) {
-                return 'background-position: ' + (-u * 32) + 'px ' + (-v * 32) + 'px; top:' + y + 'px; left: ' + x + 'px';
+        computed: {
+            manufacturers() {
+                const manufacturers = [];
+
+                let manufacturer = null;
+                for(let ship of [ ...this.ships.data.ships ].sort((a, b) => a.type.localeCompare(b.type))) {
+                    if(manufacturer && ship.manufacturer != manufacturer.name) {
+                        manufacturers.push(manufacturer);
+                        manufacturer = null;
+                    }
+
+                    if(manufacturer == null) {
+                        const id = ship.manufacturer.toUpperCase();
+
+                        manufacturer = {
+                            id,
+                            name: ship.manufacturer,
+                            info: MANUFACTURERS[id].info,
+                            ships: [ ]
+                        };
+                    }
+
+                    manufacturer.ships.push(ship);
+                }
+
+                if(manufacturer != null) {
+                    manufacturers.push(manufacturer);
+                }
+
+                return manufacturers;
             }
         }
     }
